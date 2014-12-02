4edb3288963bdc1dfdd765a37ae3103d72836773
Patrick Landin <patricklandin@gmail.com>
2014-12-01 22:50:27 -0800

    first commit

---
+++ Is it Prime?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Is it Prime?
+//
+//  Created by Patrick Landin on 12/1/14.
+//  Copyright (c) 2014 Patrick Landin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
